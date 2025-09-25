711cfd6a0fe2c319082d5a647cf2bb36dd26758c
melorin <lorinerol257@gmail.com>
2025-09-25 23:13:22 +0300

    last fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename ="/PokeLooker">
         <App />
       </BrowserRouter>
 
